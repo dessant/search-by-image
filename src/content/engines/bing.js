b02e35ff451752aa94721b0cc2d18637bf31afa7
dessant <git@rmn.space>
2017-08-26 23:31:34 +0300

    Refactor bing engine code

--- src/content/engines/bing.js
+++ src/content/engines/bing.js
@@ -1,27 +1,29 @@
+function upload(dataUri) {
+  const form = document.createElement('form');
+  form.setAttribute('data-c45ng3u9', '');
+  form.id = 'sbi-upload-form';
+  form.method = 'POST';
+  form.enctype = 'multipart/form-data';
+  form.action =
+    'https://www.bing.com/images/search?q=&view=detailv2&iss=sbiupload&FORM=IRSBIQ&redirecturl=https%3A%2F%2Fwww.bing.com%2Fimages%2Fdiscover%3Fform%3DHDRSC2#enterInsights';
+
+  const input = document.createElement('input');
+  input.setAttribute('data-c45ng3u9', '');
+  input.name = 'imageBin';
+  input.type = 'hidden';
+  input.value = dataUri.data.substring(dataUri.data.indexOf(',') + 1);
+  form.appendChild(input);
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
 function onMessage(request, sender, sendResponse) {
   if (request.id === 'dataUriResponse') {
     if (request.hasOwnProperty('error')) {
       console.log(request.error);
     } else {
-      const form = document.createElement('form');
-      form.setAttribute('data-c45ng3u9', '');
-      form.id = 'sbi-upload-form';
-      form.method = 'POST';
-      form.enctype = 'multipart/form-data';
-      form.action =
-        'https://www.bing.com/images/search?q=&view=detailv2&iss=sbiupload&FORM=IRSBIQ&redirecturl=https%3A%2F%2Fwww.bing.com%2Fimages%2Fdiscover%3Fform%3DHDRSC2#enterInsights';
-
-      const input = document.createElement('input');
-      input.setAttribute('data-c45ng3u9', '');
-      input.name = 'imageBin';
-      input.type = 'hidden';
-      input.value = request.dataUri.data.substring(
-        request.dataUri.data.indexOf(',') + 1
-      );
-      form.appendChild(input);
-
-      document.body.appendChild(form);
-      form.submit();
+      upload(request.dataUri);
     }
   }
 }
