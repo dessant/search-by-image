35de3cedbbb5d91c80db036ce1aaeb7c25639715
dessant <git@rmn.space>
2017-08-27 21:58:05 +0300

    Do not discard @font-face rules

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ module.exports = function(ctx) {
   return {
     plugins: {
       autoprefixer: ctx.env === 'production' ? {} : false,
-      cssnano: ctx.env === 'production' ? {zindex: false} : false
+      cssnano:
+        ctx.env === 'production' ? {zindex: false, discardUnused: false} : false
     }
   };
 };
