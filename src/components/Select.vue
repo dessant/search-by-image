dccd877890f6d953dc89ab94abd1c5714ca00c45
dessant <git@rmn.space>
2017-10-30 14:42:56 +0200

    fix: hide menu scrollbar
    Upstream regression:
    material-components/material-components-web/issues/1387

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -59,4 +59,8 @@ $mdc-theme-primary: #1abc9c;
 @import '@material/select/mdc-select';
 @import '@material/list/mdc-list';
 @import '@material/menu/mdc-menu';
+
+.mdc-simple-menu {
+  overflow-y: hidden !important;
+}
 </style>
