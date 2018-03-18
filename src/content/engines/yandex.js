28c7cd78a59e2aca52a33e4ab925ec22eeae3bcd
dessant <git@rmn.space>
2018-03-18 15:00:14 +0200

    fix: image upload on Yandex

--- src/content/engines/yandex.js
+++ src/content/engines/yandex.js
@@ -1,7 +1,3 @@
-function gcd(a, b) {
-  return b == 0 ? a : gcd(b, a % b);
-}
-
 function getHostname() {
   const hostnames = [
     'yandex.com',
@@ -20,36 +16,18 @@ function showResults(xhr) {
     return;
   }
 
-  const match = /(?:.*&)?cbir_id=([^&]*)(?:&.*)?/.exec(
-    JSON.parse(xhr.responseText).blocks[0].params.url
-  );
-  window.location.replace(
-    `https://${getHostname()}/images/search?` +
-      `cbir_id=${match[1]}&rpt=imageview&from=`
-  );
+  const params = JSON.parse(xhr.responseText).blocks[0].params.url;
+  window.location.replace(`https://${getHostname()}/images/search?${params}`);
 }
 
 async function upload({blob, imgData}) {
   const hostname = getHostname();
-  const sw = screen.width;
-  const sh = screen.height;
-  const r = gcd(sw, sh);
-  const ww = window.innerWidth;
-  const wh = window.innerHeight;
-  const pd = String(window.devicePixelRatio).replace('.', '_');
-  const uinfo = `sw-${sw}-sh-${sh}-ww-${ww}-wh-${wh}-pd-${pd}-wp-${sw /
-    r}x${sh / r}_${sw}x${sh}`;
-  const serpid = JSON.parse(document.querySelector('body').dataset.bem)[
-    'i-global'
-  ].serpid;
-
   const url =
-    `https://${hostname}/images/search?serpid=${serpid}&uinfo=${uinfo}` +
-    `&rpt=imageview&format=json` +
-    `&request={"blocks":[{"block":"b-page_type_search-by-image__link"}]}`;
+    `https://${hostname}/images/touch/search?rpt=imageview&format=json` +
+    `&request={"blocks":[{"block":"cbir-uploader__get-cbir-id"}]}`;
 
   const data = new FormData();
-  data.append('upfile', blob, imgData.filename);
+  data.append('upfile', blob);
 
   const xhr = getXHR();
   xhr.addEventListener('load', function() {
