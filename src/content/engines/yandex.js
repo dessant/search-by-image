23833b07412498dc3fa27583fbb85c4d5fd1048f
dessant <git@armin.dev>
2020-03-07 18:41:04 +0200

    fix: use old Yandex upload method on mobile

--- src/content/engines/yandex.js
+++ src/content/engines/yandex.js
@@ -1,21 +1,61 @@
 const engine = 'yandex';
 
+function getHostname() {
+  const hostnames = [
+    'yandex.com',
+    'yandex.ru',
+    'yandex.ua',
+    'yandex.by',
+    'yandex.kz',
+    'yandex.uz',
+    'yandex.com.tr'
+  ];
+  return getValidHostname(hostnames, engine);
+}
+
+function showResults(xhr) {
+  if (xhr.status === 413) {
+    largeImageNotify(engine, '8');
+    return;
+  }
+
+  const params = JSON.parse(xhr.responseText).blocks[0].params.url;
+  window.location.replace(`https://${getHostname()}/images/search?${params}`);
+}
+
 async function upload({blob, imgData}) {
   const button = await findNode('.input__cbir-button');
   const desktopButton = button.querySelector('button');
 
-  let input;
   if (desktopButton) {
     desktopButton.click();
 
-    input = await findNode('.cbir-panel__file-input');
+    const input = await findNode('.cbir-panel__file-input');
+
+    setFileInputData(input, blob, imgData);
+
+    input.dispatchEvent(new Event('change'));
   } else {
-    input = await findNode('.cbir-uploader__file-input');
+    const hostname = getHostname();
+    const url =
+      `https://${hostname}/images/touch/search?rpt=imageview&format=json` +
+      `&request={"blocks":[{"block":"cbir-uploader__get-cbir-id"}]}`;
+
+    const data = new FormData();
+    data.append('upfile', blob);
+
+    const xhr = getXHR();
+    xhr.addEventListener('load', function() {
+      uploadCallback(this, showResults, engine);
+    });
+    xhr.open('POST', url);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader(
+      'Accept',
+      'application/json, text/javascript, */*; q=0.01'
+    );
+    xhr.send(data);
   }
-
-  setFileInputData(input, blob, imgData);
-
-  input.dispatchEvent(new Event('change'));
 }
 
 initUpload(upload, dataKey, engine);
