e2d1048c0abdda3e390dfb2d2f1c0188a5186a23
dessant <git@armin.dev>
2021-10-15 18:12:14 +0300

    fix: update Alibaba

--- src/engines/alibabaChina.js
+++ src/engines/alibabaChina.js
@@ -4,9 +4,9 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'alibabaChina';
 
 async function search({session, search, image, storageIds}) {
-  const button = await findNode('#img-search-btn', {timeout: 120000});
+  const button = await findNode('#img-search-upload', {timeout: 120000});
 
-  const inputSelector = 'input[type=file]';
+  const inputSelector = 'input.react-file-reader-input';
   const input = await findNode(inputSelector);
   input.addEventListener('click', ev => ev.preventDefault(), {
     capture: true,
@@ -20,7 +20,7 @@ async function search({session, search, image, storageIds}) {
   await sendReceipt(storageIds);
 
   window.setTimeout(() => {
-    input.dispatchEvent(new Event('change'));
+    input.dispatchEvent(new Event('change', {bubbles: true}));
   }, 100);
 }
 
