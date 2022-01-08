ca7eb1c7e022afb897cbcd023f6b2b72af9491c5
dessant <git@armin.dev>
2022-01-08 17:46:40 +0200

    fix: update Getty

--- src/engines/getty.js
+++ src/engines/getty.js
@@ -6,7 +6,7 @@ const engine = 'getty';
 async function search({session, search, image, storageIds}) {
   (await findNode('button[class*="SearchByImageButton"]')).click();
 
-  const inputSelector = 'input[type=file]';
+  const inputSelector = 'input[type=file][data-testid="drag-drop-input"]';
   const input = await findNode(inputSelector);
 
   await setFileInputData(inputSelector, input, image);
