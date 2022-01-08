003d52dfbcc4bddf117761dd57a6754382bb49c2
dessant <git@armin.dev>
2022-01-08 17:46:17 +0200

    fix: update Dreamstime

--- src/engines/dreamstime.js
+++ src/engines/dreamstime.js
@@ -1,9 +1,18 @@
-import {findNode} from 'utils/common';
+import {findNode, isMobile} from 'utils/common';
 import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
+import {targetEnv} from 'utils/config';
 
 const engine = 'dreamstime';
 
 async function search({session, search, image, storageIds}) {
+  if (targetEnv === 'safari' && (await isMobile())) {
+    // hide noncritical upload error
+    const script = document.createElement('script');
+    script.textContent = 'window.alert = function () {}';
+    document.documentElement.appendChild(script);
+    script.remove();
+  }
+
   const inputSelector = 'input.puzzle-file';
   const input = await findNode(inputSelector);
 
