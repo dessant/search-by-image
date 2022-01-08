ba4a525b48c15a282a1869f04ba54c4ff4249725
dessant <git@armin.dev>
2022-01-08 17:45:30 +0200

    fix: update Depositphotos

--- src/engines/depositphotos.js
+++ src/engines/depositphotos.js
@@ -1,52 +1,23 @@
-import {findNode, isMobile} from 'utils/common';
-import {validateUrl} from 'utils/app';
+import {findNode} from 'utils/common';
 import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 
 const engine = 'depositphotos';
 
 async function search({session, search, image, storageIds}) {
-  if (await isMobile()) {
-    const data = new FormData();
-    data.append('file', image.imageBlob, image.imageFilename);
+  (
+    await findNode('body:not(.preload) button[data-qa="SearchByImageButton"]', {
+      observerOptions: {attributes: true, attributeFilter: ['class']}
+    })
+  ).click();
 
-    const rsp = await fetch('https://msis.depositphotos.com/search', {
-      mode: 'cors',
-      method: 'POST',
-      body: data
-    });
+  const inputSelector = 'input[type=file]';
+  const input = await findNode(inputSelector);
 
-    if (rsp.status !== 200) {
-      throw new Error(`API response: ${rsp.status}, ${await rsp.text()}`);
-    }
+  await setFileInputData(inputSelector, input, image);
 
-    const results = (await rsp.json()).data.items;
-    const ids = encodeURIComponent(results.map(item => item.id).join(','));
+  await sendReceipt(storageIds);
 
-    const tabUrl = `https://depositphotos.com/search/by-images.html?idList=[${ids}]`;
-
-    await sendReceipt(storageIds);
-
-    if (validateUrl(tabUrl)) {
-      window.location.replace(tabUrl);
-    }
-  } else {
-    (
-      await findNode(
-        'body:not(.preload) div.enable-transition button.button-search-by-images'
-      )
-    ).click();
-
-    (await findNode('li[data-key="image"] a.cmp-tabs__label')).click();
-
-    const inputSelector = 'input[type=file]';
-    const input = await findNode(inputSelector);
-
-    await setFileInputData(inputSelector, input, image);
-
-    await sendReceipt(storageIds);
-
-    input.dispatchEvent(new Event('change', {bubbles: true}));
-  }
+  input.dispatchEvent(new Event('change', {bubbles: true}));
 }
 
 function init() {
