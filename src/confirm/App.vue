bae6738bb3d4657844bc911b9cba66175eddf560
dessant <git@armin.dev>
2022-05-04 18:35:49 +0300

    feat: show image dimensions in selection dialog
    Closes #185.

--- src/confirm/App.vue
+++ src/confirm/App.vue
@@ -6,26 +6,66 @@
     :show-dialog="showDialog"
     @cancel="onCancel"
   >
-    <div class="mdc-grid-list">
-      <ul class="mdc-grid-list__tiles">
-        <li class="mdc-grid-tile" v-for="(img, index) in images" :key="index">
-          <div class="mdc-grid-tile__primary">
-            <div
-              class="mdc-grid-tile__primary-content tile-container"
-              tabindex="0"
-              :data-index="index"
-              @click="onSelection"
-              @keyup.enter="onSelection"
-            >
+    <div class="preview-images">
+      <div
+        class="tile-container"
+        tabindex="0"
+        :data-index="index"
+        @click="onSelection"
+        @keyup.enter="onSelection"
+        v-for="(img, index) in previewImages"
+        :key="index"
+      >
+        <picture class="image-container">
+          <source :srcset="img.image.imageDataUrl || img.image.imageUrl" />
+          <img
+            class="image"
+            referrerpolicy="no-referrer"
+            src="/src/assets/icons/misc/broken-image.svg"
+            :data-index="index"
+            @error.once="onPreviewImageError"
+            @load="onPreviewImageLoad"
+            :alt="img.image.imageFilename"
+          />
+        </picture>
+
+        <div class="image-details" v-if="imagesLoaded">
+          <div class="image-size">
+            <div class="image-size-icons">
               <img
-                class="tile"
-                referrerpolicy="no-referrer"
-                :src="img.imageDataUrl || img.imageUrl"
+                class="image-size-icon"
+                src="/src/assets/icons/misc/largest-image-width.svg"
+                v-if="
+                  largestImageDimension &&
+                  img.imageWidth === largestImageWidth &&
+                  img.imageHeight < largestImageHeight
+                "
+              />
+              <img
+                class="image-size-icon"
+                src="/src/assets/icons/misc/largest-image-height.svg"
+                v-if="
+                  largestImageDimension &&
+                  img.imageHeight === largestImageHeight &&
+                  img.imageWidth < largestImageWidth
+                "
+              />
+              <img
+                class="image-size-icon"
+                src="/src/assets/icons/misc/largest-image-dimension.svg"
+                v-if="
+                  largestImageDimension &&
+                  img.imageDimension === largestImageDimension
+                "
               />
             </div>
+
+            <div class="image-size-text" v-if="img.imageWidth">
+              {{ img.imageWidth }} × {{ img.imageHeight }}
+            </div>
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   </v-dialog>
 </template>
@@ -47,7 +87,12 @@ export default {
       contentMessagePort: null,
 
       session: null,
-      images: []
+      previewImages: [],
+
+      imagesLoaded: false,
+      largestImageWidth: 0,
+      largestImageHeight: 0,
+      largestImageDimension: 0
     };
   },
 
@@ -62,23 +107,25 @@ export default {
       }
 
       if (request.id === 'openView') {
-        this.session = request.session;
-        this.images = request.images;
-        this.showDialog = true;
+        this.setup(request.session, request.images);
+        this.openDialog();
       } else if (request.id === 'closeView') {
-        this.showDialog = false;
+        this.closeDialog();
       }
     },
 
     onCancel: function () {
-      this.showDialog = false;
+      this.closeDialog();
       browser.runtime.sendMessage({id: 'cancelView', view: 'confirm'});
     },
 
     onSelection: async function (ev) {
-      this.showDialog = false;
+      this.closeDialog();
 
-      const image = Object.assign({}, this.images[ev.target.dataset.index]);
+      const image = Object.assign(
+        {},
+        this.previewImages[ev.currentTarget.dataset.index].image
+      );
 
       if (this.session.sessionType === 'share') {
         await shareImage(image, {
@@ -92,31 +139,118 @@ export default {
           session: this.session
         });
       }
+    },
+
+    onPreviewImageLoad: function (ev) {
+      const index = ev.target.dataset.index;
+
+      const src = ev.target.currentSrc;
+      if (src.startsWith('data') || src.startsWith('http')) {
+        this.setPreviewImageDetails(index, {
+          width: ev.target.naturalWidth,
+          height: ev.target.naturalHeight
+        });
+      }
+
+      this.setPreviewImageLoaded(index);
+    },
+
+    onPreviewImageError: function (ev) {
+      const source = ev.target.previousElementSibling;
+      source.srcset = ev.target.src;
+      source.type = 'image/svg+xml';
+
+      this.setPreviewImageLoaded(ev.target.dataset.index);
+    },
+
+    setPreviewImageDetails: function (index, {width = 0, height = 0} = {}) {
+      const image = this.previewImages[index];
+      image.imageDimension = width * height;
+      image.imageWidth = width;
+      image.imageHeight = height;
+
+      this.setLargestImageDimension();
+    },
+
+    setPreviewImageLoaded: function (index) {
+      const image = this.previewImages[index];
+      image.imageLoaded = true;
+
+      this.setImagesLoaded();
+    },
+
+    setLargestImageDimension: function () {
+      for (const image of this.previewImages) {
+        if (image.imageDimension > this.largestImageDimension) {
+          this.largestImageDimension = image.imageDimension;
+        }
+
+        if (image.imageWidth > this.largestImageWidth) {
+          this.largestImageWidth = image.imageWidth;
+        }
+
+        if (image.imageHeight > this.largestImageHeight) {
+          this.largestImageHeight = image.imageHeight;
+        }
+      }
+    },
+
+    setImagesLoaded: function () {
+      this.imagesLoaded = this.previewImages.every(image => image.imageLoaded);
+    },
+
+    addPreviewImages: function (images) {
+      this.previewImages = images.map(image => ({
+        image,
+        imageWidth: 0,
+        imageHeight: 0,
+        imageDimension: 0,
+        imageLoaded: false
+      }));
+    },
+
+    openDialog: function () {
+      this.showDialog = true;
+    },
+
+    closeDialog: function () {
+      this.showDialog = false;
+    },
+
+    setup: function (session, images) {
+      this.imagesLoaded = false;
+      this.largestImageWidth = 0;
+      this.largestImageHeight = 0;
+      this.largestImageDimension = 0;
+
+      this.session = session;
+      this.addPreviewImages(images);
+    },
+
+    init: async function () {
+      if (this.$env.isSafari) {
+        const tab = await browser.tabs.getCurrent();
+        this.contentMessagePort = browser.tabs.connect(tab.id, {frameId: 0});
+        this.contentMessagePort.onMessage.addListener(this.onMessage);
+      } else {
+        browser.runtime.onMessage.addListener(this.onMessage);
+        browser.runtime.sendMessage({
+          id: 'routeMessage',
+          setSenderFrameId: true,
+          messageFrameId: 0,
+          message: {id: 'saveFrameId'}
+        });
+      }
     }
   },
 
-  mounted: async function () {
-    if (this.$env.isSafari) {
-      const tab = await browser.tabs.getCurrent();
-      this.contentMessagePort = browser.tabs.connect(tab.id, {frameId: 0});
-      this.contentMessagePort.onMessage.addListener(this.onMessage);
-    } else {
-      browser.runtime.onMessage.addListener(this.onMessage);
-      browser.runtime.sendMessage({
-        id: 'routeMessage',
-        setSenderFrameId: true,
-        messageFrameId: 0,
-        message: {id: 'saveFrameId'}
-      });
-    }
+  mounted: function () {
+    this.init();
   }
 };
 </script>
 
 <style lang="scss">
-$mdc-theme-primary: #1abc9c;
-
-@import '@material/grid-list/mdc-grid-list';
 @import '@material/button/mixins';
 @import '@material/ripple/mixins';
 @import '@material/theme/mixins';
@@ -127,18 +261,35 @@ body {
   font-size: 100%;
 }
 
-.tile-container {
+.preview-images {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-wrap: wrap;
+  gap: 16px;
+
+  max-width: calc(200px + 24px);
+  @media (min-width: 520px) {
+    max-width: calc(416px + 24px);
+  }
+  @media (min-width: 736px) {
+    max-width: calc(632px + 24px);
+  }
+}
+
+.tile-container {
+  width: 200px;
+  height: 186px;
+  padding: 8px;
+  box-sizing: border-box;
+  position: relative;
   cursor: pointer;
+  z-index: 1;
 }
 
 .tile-container:focus {
   outline: 0;
 }
 
-.tile-container::after {
+.tile-container::before {
   content: '';
   position: absolute;
   top: 0;
@@ -147,25 +298,68 @@ body {
   left: 0;
   width: 100%;
   height: 100%;
-  opacity: 0;
+  z-index: -1;
   background-color: #bdc3c7;
-  transition: all 0.3s ease;
-  border-radius: 8px !important;
+  border-radius: 8px;
+  transition: all 0.2s ease;
+  transform: scale(0.96);
+  opacity: 0;
 }
 
-.tile-container:focus::after,
-.tile-container:hover::after {
-  opacity: 0.16;
+.tile-container:focus::before,
+.tile-container:hover::before {
+  transform: scale(1);
+  opacity: 0.26;
 }
 
-.tile {
-  max-width: 94%;
-  max-height: 94%;
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 138px;
+}
+
+.image {
+  display: flex;
   object-fit: scale-down;
+  max-width: 100%;
+  max-height: 138px;
+}
+
+.image-details {
+  margin-top: 8px;
+}
+
+.image-size {
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+  height: 24px;
+}
+
+.image-size-icons {
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+}
+
+.image-size-icon {
+  width: 16px;
+  height: 16px;
+  opacity: 0.8;
+}
+
+.image-size-text {
+  @include mdc-typography(caption);
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 
 .mdc-dialog__surface {
   border-radius: 16px !important;
+  min-width: initial !important;
+  max-width: initial !important;
 }
 
 .mdc-dialog__title {
@@ -184,16 +378,6 @@ body {
 .safari {
   & .mdc-dialog__button {
     -webkit-mask-image: -webkit-radial-gradient(white, black);
-  }
-}
-
-@media (min-width: 722px) {
-  .mdc-dialog__surface {
-    max-width: 660px !important;
-  }
-
-  .mdc-dialog--scrollable .mdc-dialog__surface {
-    max-width: 690px !important;
   }
 }
 </style>
