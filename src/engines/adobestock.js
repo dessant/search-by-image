49a089756ef2823e0cbc14685b7dfba8d7c053b8
dessant <git@armin.dev>
2022-08-06 12:52:35 +0300

    fix: update Adobe Stock

--- src/engines/adobestock.js
+++ src/engines/adobestock.js
@@ -4,16 +4,16 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'adobestock';
 
 async function search({session, search, image, storageIds}) {
-  (await findNode('button.js-camera-icon')).click();
+  (await findNode('button[aria-label="Visual Search"]')).click();
 
-  const inputSelector = '#js-vsupload';
+  const inputSelector = 'input[type=file]';
   const input = await findNode(inputSelector);
 
   await setFileInputData(inputSelector, input, image);
 
   await sendReceipt(storageIds);
 
-  input.dispatchEvent(new Event('change'));
+  input.dispatchEvent(new Event('change', {bubbles: true}));
 }
 
 function init() {
