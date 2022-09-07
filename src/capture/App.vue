25bd573d7c71d121e3a8712d31728fc9f8553d90
dessant <git@armin.dev>
2022-09-07 15:19:48 +0300

    fix: update toolbar style

--- src/capture/App.vue
+++ src/capture/App.vue
@@ -346,6 +346,7 @@ body {
 .mdc-snackbar__surface {
   box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.12),
     0px 6px 10px 0px rgba(0, 0, 0, 0.08), 0px 1px 12px 0px rgba(0, 0, 0, 0.06);
+  border-radius: 16px !important;
 }
 
 .mdc-snackbar__label {
