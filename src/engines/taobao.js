c07c1d7652903f7cf577c72317a27a9767b431f2
dessant <git@armin.dev>
2023-05-31 13:15:04 +0300

    fix: update Taobao

--- src/engines/taobao.js
+++ src/engines/taobao.js
@@ -4,16 +4,45 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'taobao';
 
 async function search({session, search, image, storageIds}) {
-  const button = await findNode('div.drop-wrapper', {timeout: 120000});
+  function patchContext() {
+    const appendChildFn = Element.prototype.appendChild;
+    Element.prototype.appendChild = function (node) {
+      if (node.type === 'file') {
+        node.addEventListener('click', ev => ev.preventDefault(), {
+          capture: true,
+          once: true
+        });
 
-  const inputSelector = 'input#J_IMGSeachUploadBtn';
+        Element.prototype.appendChild = appendChildFn;
+      }
+
+      return appendChildFn.apply(this, arguments);
+    };
+
+    const openFn = window.open;
+    window.open = function (url) {
+      if (url.includes('/tmw/search_image')) {
+        window.location.replace(url);
+      } else {
+        return openFn.apply(this, arguments);
+      }
+    };
+  }
+
+  const script = document.createElement('script');
+  script.textContent = `(${patchContext.toString()})()`;
+  document.documentElement.appendChild(script);
+  script.remove();
+
+  (
+    await findNode(
+      '.searchbar-camera-icon div.component-search-icon-container',
+      {timeout: 120000}
+    )
+  ).click();
+
+  const inputSelector = 'input[type="file"]';
   const input = await findNode(inputSelector);
-  input.addEventListener('click', ev => ev.preventDefault(), {
-    capture: true,
-    once: true
-  });
-
-  button.click();
 
   await setFileInputData(inputSelector, input, image);
 
