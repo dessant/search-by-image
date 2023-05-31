9834315d3031dcec12335dbbccc37e47973afe67
dessant <git@armin.dev>
2023-05-31 13:06:27 +0300

    fix: update Dreamstime

--- src/engines/dreamstime.js
+++ src/engines/dreamstime.js
@@ -1,4 +1,4 @@
-import {findNode, isMobile} from 'utils/common';
+import {findNode, processNode, isMobile} from 'utils/common';
 import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 import {targetEnv} from 'utils/config';
 
@@ -13,10 +13,20 @@ async function search({session, search, image, storageIds}) {
     script.remove();
   }
 
-  const inputSelector = 'input.puzzle-file';
+  const inputSelector = 'input[type="file"]';
+
+  processNode(inputSelector, function (node) {
+    node.addEventListener('click', ev => ev.preventDefault(), {
+      capture: true,
+      once: true
+    });
+  });
+
+  (await findNode('button.search-by-image__btn')).click();
+
   const input = await findNode(inputSelector);
 
-  await setFileInputData(inputSelector, input, image);
+  await setFileInputData(inputSelector, input, image, {patchInput: true});
 
   await sendReceipt(storageIds);
 
