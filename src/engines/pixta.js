9624708e706c00a9e11edb37a4d0cab5fcbed324
dessant <git@armin.dev>
2023-12-10 15:43:13 +0200

    fix: update PIXTA

--- src/engines/pixta.js
+++ src/engines/pixta.js
@@ -4,9 +4,23 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'pixta';
 
 async function search({session, search, image, storageIds}) {
-  (await findNode('div.search-image-button.search-image-button--top')).click();
+  const mobileLayout = window.matchMedia('(max-width: 768px)').matches;
 
-  const inputSelector = 'input#image[type="file"]';
+  if (mobileLayout) {
+    (await findNode('div.global-header-sp i.fa-search')).click();
+
+    (
+      await findNode('.global-search-form-sp__search-by-image-btn i.fa-camera')
+    ).click();
+  } else {
+    (
+      await findNode('div.search-image-button.search-image-button--top')
+    ).click();
+  }
+
+  const inputSelector = mobileLayout
+    ? 'input#upload-photo[type="file"]'
+    : 'input#image[type="file"]';
   const input = await findNode(inputSelector);
 
   await setFileInputData(inputSelector, input, image);
