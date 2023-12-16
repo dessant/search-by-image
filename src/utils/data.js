7d46424fd521935419b73ca960178dc0e5998cad
dessant <git@armin.dev>
2023-12-16 15:14:47 +0200

    chore: update Chrome user agent

--- src/utils/data.js
+++ src/utils/data.js
@@ -691,10 +691,10 @@ const maxImageUploadSize = {
 };
 
 const chromeDesktopUA =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36';
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 
 const chromeMobileUA =
-  'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36';
+  'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36';
 
 const chromeSbiSrc = 'Google Chrome 110.0.5481.78 (Official) Windows';
 
