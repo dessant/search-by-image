850b3e3e212b571e7fbe48f70c768b2abbbdeb21
dessant <git@armin.dev>
2024-12-11 19:11:52 +0200

    fix: update Shein
    Closes #329.

--- src/engines/shein.js
+++ src/engines/shein.js
@@ -9,16 +9,7 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'shein';
 
 async function search({session, search, image, storageIds}) {
-  // close upload progress popup
-  processNode('.c-modal-wrap[tip="uploading"] button', node => node.click(), {
-    throwError: false
-  });
-  // close promotion popup
-  processNode('.in-pop-inner button.btn-shop-now', node => node.click(), {
-    throwError: false
-  });
-
-  (await findNode('i.search-upload__icon')).click();
+  (await findNode('.search-input_upload span.sui-icon-common__wrap')).click();
 
   const inputSelector = '.search-upload input[type="file"]';
   const input = await findNode(inputSelector);
