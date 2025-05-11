3e51fb5eb2e2aa73933b83831c4b1cd4ee9ddd6b
dessant <git@armin.dev>
2025-05-11 17:27:36 +0300

    fix: update Freepik

--- src/engines/freepik.js
+++ src/engines/freepik.js
@@ -4,7 +4,9 @@ import {setFileInputData, initSearch, sendReceipt} from 'utils/engines';
 const engine = 'freepik';
 
 async function search({session, search, image, storageIds}) {
-  (await findNode('form > div > button[data-state="closed"]')).click();
+  (
+    await findNode('form > div > button[data-state="closed"]:last-of-type')
+  ).click();
 
   const inputSelector = 'input[type=file]';
   const input = await findNode(inputSelector);
@@ -15,12 +17,7 @@ async function search({session, search, image, storageIds}) {
 
   input.dispatchEvent(new Event('change', {bubbles: true}));
 
-  (
-    await findNode(
-      'button[data-cy="home-hero-bar-upload-modal-submit"]:not([disabled])',
-      {observerOptions: {attributes: true}}
-    )
-  ).click();
+  (await findNode('.bg-surface-1 button[type=submit]')).click();
 }
 
 function init() {
