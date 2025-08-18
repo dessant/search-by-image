97496e9102bb8635ae9d4bb196357286e777cc4a
dessant <git@armin.dev>
2025-08-18 10:29:00 +0300

    fix: update Wildberries

--- src/engines/wildberries.js
+++ src/engines/wildberries.js
@@ -7,14 +7,28 @@ async function search({session, search, image, storageIds}) {
   // wait for search service to load
   await sleep(1000);
 
-  const inputSelector = 'input[type=file][data-link*="searchByImage"]';
+  const button = await findNode('div#searchByImageFormAbNew');
+  const isNewSearchForm = !button.classList.contains('hide');
+
+  if (isNewSearchForm) {
+    button.click();
+  }
+
+  const inputSelector = isNewSearchForm
+    ? 'input#popUpFileInput'
+    : 'input.j-photo-search__label';
   const input = await findNode(inputSelector);
 
   await setFileInputData(inputSelector, input, image);
 
   await sendReceipt(storageIds);
 
-  input.dispatchEvent(new Event('change'));
+  input.dispatchEvent(new Event('change', {bubbles: true}));
+
+  // wait for image to load
+  await sleep(1000);
+
+  (await findNode('button#searchGoodsButton')).click();
 }
 
 function init() {
